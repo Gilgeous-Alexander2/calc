7bea47bd8a744b3a7666fb11ea8705ef4f53ccbc
Gilgeous-Alexander2 <117025505+Gilgeous-Alexander2@users.noreply.github.com>
2023-02-27 13:44:43 +0300

    dobavil matplot

--- testgit.py
+++ testgit.py
@@ -1,5 +1,9 @@
-sdgsdssASSsfd
-jhegjhhrjjkarnjfdjna
-dfbdfzkghjjghbknjnjlnl
-ромимиоррооро
-вауфаиффив
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.linespace(8,2*np.pi,200)
+y=np.sin(x)
+
+fig,ax=plt.subplots()
+ax.plot(x,y)
+plt.show()
