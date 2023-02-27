b515ff5e9876d72ea449c78111c058bbebca59ed
Gilgeous-Alexander2 <117025505+Gilgeous-Alexander2@users.noreply.github.com>
2023-02-27 13:08:28 +0300

    sdelal

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,23 @@
-print("hello")
+from function import inputnum
+
+f=""
+
+while f!="нет":
+    try:
+        s = input("Знак (+,-,*,/): ")
+        if s in ('+', '-', '*', '/'):
+            x = inputnum("x")
+            y = inputnum("y")
+            if s == '+':
+                print("%.2f" % (x+y))
+            elif s == '-':
+                print("%.2f" % (x-y))
+            elif s == '*':
+                print("%.2f" % (x*y))
+            elif s == '/':
+                print("%.2f" % (x/y))
+        print("Еще?(да/нет)")
+        f=input()
+    except:
+            print("не надо так")
+
